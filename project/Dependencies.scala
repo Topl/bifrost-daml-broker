8cf3860426cda94da614fe13391aba5782946dd7
Edmundo Lopez <e.lopez@topl.me>
2023-02-23 16:59:14 -0400

    TSDK-320 Fix parameters and logging

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,8 +10,7 @@ object Dependencies {
   lazy val brambl = toplOrg %% "brambl" % bramblVersion
   lazy val bramblCommon = toplOrg %% "common" % bramblVersion
   lazy val catEffects = "org.typelevel" %% "cats-effect" % "3.3.12"
-  lazy val toplDaml = toplOrg % "daml-bifrost-module" % "0.1.0"
-  lazy val slf4j = "org.slf4j" % "slf4j-simple" % "2.0.5"
+  lazy val toplDaml = toplOrg % "daml-bifrost-module" % "0.1.0-SNAPSHOT"
   lazy val scopt = "com.github.scopt" %% "scopt" % "4.0.1"
   lazy val munit = "org.scalameta" %% "munit" % "0.7.29" % Test
   lazy val fs2Core = "co.fs2" %% "fs2-core" % "3.5.0"
