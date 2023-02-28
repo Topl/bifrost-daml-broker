f198d01938c6ff471a8fce254058989ae45aefbd
Edmundo Lopez <e.lopez@topl.me>
2023-02-28 14:25:43 -0400

    TSDK-320 Fix dependency

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
   lazy val brambl = toplOrg %% "brambl" % bramblVersion
   lazy val bramblCommon = toplOrg %% "common" % bramblVersion
   lazy val catEffects = "org.typelevel" %% "cats-effect" % "3.3.12"
-  lazy val toplDaml = toplOrg % "daml-bifrost-module" % "0.1.0-SNAPSHOT"
+  lazy val toplDaml = toplOrg % "daml-bifrost-module" % "0.1.0"
   lazy val scopt = "com.github.scopt" %% "scopt" % "4.0.1"
   lazy val munit = "org.scalameta" %% "munit" % "0.7.29" % Test
   lazy val fs2Core = "co.fs2" %% "fs2-core" % "3.5.0"
